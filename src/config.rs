4e7f54924fe9a87cdf3a380da9536f333c977cb0
pinepal <124350776+nico359@users.noreply.github.com>
2026-06-24 11:29:55 +0200

    Bump version to 0.3.2

--- src/config.rs
+++ src/config.rs
@@ -1,4 +1,4 @@
-pub static VERSION: &str = "0.3.1";
+pub static VERSION: &str = "0.3.2";
 pub static GETTEXT_PACKAGE: &str = "pinepal";
 pub static LOCALEDIR: &str = "/app/share/locale";
 pub static PKGDATADIR: &str = "/app/share/pinepal";
